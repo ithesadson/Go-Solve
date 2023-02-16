06edc355881e156837b3e09d2324ed4106f0423d
ithesadson <thesadson@gmail.com>
2023-02-16 23:18:07 +0300

    Comment lines edited

--- Chocolate_in_Box/Chocolate.go
+++ Chocolate_in_Box/Chocolate.go
@@ -3,45 +3,50 @@ package main
 import "fmt"
 
 func main() {
-	// This question part of Nim game Strategy
+	var n int
+	//How many elements will the array have
+	fmt.Scan(&n)
+	//Define an array of "n" elements
+	arr := make([]int32, n)
 
-	// For Default test-case | Sample Input 2 3
-	//  a | 2 = 10 (binary)
-	//  b | 3 = 11 (binary) --> a+b = binary(01) = 1 | allxor=1
-
-	arr := [2]int32{2, 3}
-	var win_counter int32 = 0
-	var allxor int32 = 0
-	N := len(arr)
-
-	for i := 0; i < N; i++ {
-		allxor ^= arr[i]
+	for i := 0; i < n; i++ {
+		//Enter the elements of the array
+		fmt.Scan(&arr[i])
 	}
-
-	for j := 0; j < N; j++ {
-		//For player 1 to win, the array element must be greater than element * allxor.
-		if arr[j] > (allxor ^ arr[j]) {
-			win_counter += 1
-		}
-	}
-	fmt.Println(win_counter)
+	fmt.Println("Sample Output:", chocolateInBox(arr))
 }
 
-/* Function solution
 func chocolateInBox(arr []int32) int32 {
-	// This question part of Nim game Strategy
+	//This function solution includes a nim game strategy
 	var win_counter int32 = 0
-	var allxor int32 = 0
+	var allxor int32 = 0 // XOR logic operator
 	N := len(arr)
-
 	for i := 0; i < N; i++ {
+		//Sum of array elements with XOR Gate
 		allxor ^= arr[i]
 	}
 	for i := 0; i < N; i++ {
+		//Calculation of probabilities leading to a decisive win.
+		//Applying and comparing the xor gate for all elements.
 		if arr[i] > (allxor ^ arr[i]) {
 			win_counter += 1
 		}
 	}
 	return win_counter
 }
+
+/* Test Case ->
+Input (stdin)
+2
+2 3
+
+Expected Output
+1
+---------------
+Input (stdin)
+5
+251149 86127 711523 501067 617190
+
+Expected Output
+1
 */
